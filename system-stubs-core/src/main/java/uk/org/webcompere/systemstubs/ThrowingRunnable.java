17c1af16d36784240872b1788240993aa6fdf676
Ashley Frieze <ashley@incredible.org.uk>
2020-11-09 09:23:58 +0000

    Add the basics of the resource pattern
    - to allow each of the tools to be turned on and off as needed
    - apply to environment variables first

--- system-stubs-core/src/main/java/uk/org/webcompere/systemstubs/ThrowingRunnable.java
+++ system-stubs-core/src/main/java/uk/org/webcompere/systemstubs/ThrowingRunnable.java
@@ -1,5 +1,7 @@
 package uk.org.webcompere.systemstubs;
 
+import java.util.concurrent.Callable;
+
 /**
  * Code that should be executed by on of the methods of {@link SystemStubs}.
  * This code may throw an {@link Exception}. Therefore we cannot use
@@ -12,4 +14,15 @@ public interface ThrowingRunnable {
      * @throws Exception the action may throw an arbitrary exception.
      */
     void run() throws Exception;
+
+    /**
+     * Convert to a Callable
+     * @return a callable which executes this
+     */
+    default Callable<Void> asCallable() {
+        return () -> {
+          run();
+          return null;
+        };
+    }
 }
